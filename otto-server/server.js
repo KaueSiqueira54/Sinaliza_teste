dbab65fd6bf2d2f12e88a447456776cc30b4cb40
KaueSiqueira <kaue54150@gmail.com>
2026-06-10 16:23:02 -0300

    teste 03

--- otto-server/server.js
+++ otto-server/server.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://sinalizaai.netlify.app",
-      "https://sinali.netlify.app/",
+      "https://sinali.netlify.app",
     ],
   }),
 );
